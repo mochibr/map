e5a14ade6479abc1d2075731ce1dafe0f4d466b8
mochibr <bittu.w3web@gmail.com>
2022-05-17 18:08:20 +0530

    set the code

--- data.js
+++ data.js
@@ -1,9 +1,54 @@
 let data = [
-		{title: 'Gujarat',		lat: 22.25,		lng: 71.19, lineColor: '#ff0000', flag: 'http://maps.google.co.uk/intl/en_ALL/mapfiles/ms/micons/green-dot.png', price: 25, DOP:"10/10/99", Name: "Amana"},
-		{title: 'Lucknow',		lat: 26.84,		lng: 80.94, lineColor: '#4eff00', flag: 'http://maps.google.co.uk/intl/en_ALL/mapfiles/ms/micons/green-dot.png', price: 254, DOP:"10/10/99", Name: "Khan"},
-		{title: 'Delhi',lat: 28.70,	lng: 77.10, lineColor: '#00ffd3', flag: 'http://maps.google.co.uk/intl/en_ALL/mapfiles/ms/micons/green-dot.png', price: 525, DOP:"10/10/99", Name: "Jim"},
-		{title: 'Jaipur',		lat: 26.91,		lng: 75.78, lineColor: '#0b00ff', flag: 'http://maps.google.co.uk/intl/en_ALL/mapfiles/ms/micons/green-dot.png', price: 825, DOP:"10/10/99", Name: "Amit"},
-		{title: 'Indore',		lat: 22.71,		lng: 75.85, lineColor: '#ffef00', flag: 'http://maps.google.co.uk/intl/en_ALL/mapfiles/ms/micons/green-dot.png', price: 725, DOP:"10/10/99", Name: "Raj"}
+  {
+    title: "Gujarat",
+    lat: 22.25,
+    lng: 71.19,
+    lineColor: "#ff0000",
+    flag: "http://maps.google.co.uk/intl/en_ALL/mapfiles/ms/micons/green-dot.png",
+    price: 25,
+    DOP: "10/10/99",
+    Name: "Amana",
+  },
+  {
+    title: "Lucknow",
+    lat: 26.84,
+    lng: 80.94,
+    lineColor: "#4eff00",
+    flag: "http://maps.google.co.uk/intl/en_ALL/mapfiles/ms/micons/green-dot.png",
+    price: 254,
+    DOP: "10/10/99",
+    Name: "Khan",
+  },
+  {
+    title: "Delhi",
+    lat: 28.7,
+    lng: 77.1,
+    lineColor: "#00ffd3",
+    flag: "http://maps.google.co.uk/intl/en_ALL/mapfiles/ms/micons/green-dot.png",
+    price: 525,
+    DOP: "10/10/99",
+    Name: "Jim",
+  },
+  {
+    title: "Jaipur",
+    lat: 26.91,
+    lng: 75.78,
+    lineColor: "#0b00ff",
+    flag: "http://maps.google.co.uk/intl/en_ALL/mapfiles/ms/micons/green-dot.png",
+    price: 825,
+    DOP: "10/10/99",
+    Name: "Amit",
+  },
+  {
+    title: "Indore",
+    lat: 22.71,
+    lng: 75.85,
+    lineColor: "#ffef00",
+    flag: "http://maps.google.co.uk/intl/en_ALL/mapfiles/ms/micons/green-dot.png",
+    price: 725,
+    DOP: "10/10/99",
+    Name: "Raj",
+  },
 ];
 
-let Home = {title: 'Chennai', lat: 13.08, lng: 80.27};
+let Home = { title: "Chennai", lat: 13.08, lng: 80.27 };
